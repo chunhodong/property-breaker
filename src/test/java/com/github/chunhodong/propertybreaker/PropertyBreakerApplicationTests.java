e0f55dcce3851d5a771c3f504af973eddc60e20f
chunhodong <yhms4432@naver.com>
2022-09-15 15:10:03 +0900

    first commit

---
+++ src/test/java/com/github/chunhodong/propertybreaker/PropertyBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.chunhodong.propertybreaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
